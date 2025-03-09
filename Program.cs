996dfaf0f0902a6796a060aa3365b5e23bd847cd
vinicius aquino oliveira <viniusername@gmail.com>
2025-03-09 15:12:31 -0300

    Resolvido o exercicio

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿Console.BackgroundColor=ConsoleColor.DarkGreen;
+Console.ForegroundColor=ConsoleColor.Blue;
+Console.Write("Aaron Smith - Dancin (KRONO Remix)");
+Console.ResetColor();
+Console.WriteLine();
+Console.ForegroundColor=ConsoleColor.Blue;
+Console.Write(@"Get up on the floor
+Dancin' all night long
+Get up on the floor
+Dancin' till the break of dawn
+Get up on the floor
+Dancin' till the break of dawn
+Get up on the floor
+Dancin'
+");
+Console.ResetColor();
+Console.WriteLine();
+Console.Write(@"All the time
+My baby you on my mind
+And I don't know why
+Yeah but the feeling is fine
+Can't you see
+Honey you are for me
+Though it weren't meant to be
+
+Dancin' in the moonlight
+Gazing at the stars so bright
+Holding you until the sunrise
+Sleeping until the midnight
+
+Everytime when I look in your eyes
+That smile was crying that you were mine
+Do you know, your love is true I know
+You are the best thing that has happened to me
+
+Get up on the floor
+Dancin' all night long
+Get up on the floor
+Dancin' till the break of dawn
+Get up on the floor
+Dancin' till the break of dawn
+Get up on the floor
+Dancin'
+
+Dancin' is what to do
+Dancin's when I think of you
+Dancin's what clears my soul
+Dancin's what makes me whole
+Dancin' is what to do
+Dancin's when I think of you
+Dancin's what clears my soul
+Dancin's what makes me whole  (3x)
+");
+Console.WriteLine();
+Console.ForegroundColor=ConsoleColor.Blue;
+Console.Write(@"Get up on the floor
+Dancin' all night long
+Get up on the floor
+Dancin' till the break of dawn
+Get up on the floor
+Dancin' till the break of dawn
+Get up on the floor");
+Console.ResetColor();
+Console.ForegroundColor=ConsoleColor.DarkBlue;
+Console.WriteLine();
+Console.WriteLine();
+Console.Write(@"Keyboard keys by Hayley Jane Wakenshaw
+ _______________________________________________________________
+|| | | ||| | ||| | | ||| | ||| | | ||| | ||| | | ||| | ||| | | ||
+||_|_|_|||_|_|||_|_|_|||_|_|||_|_|_|||_|_|||_|_|_|||_|_|||_|_|_||
+| | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |
+|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|");
+Console.ResetColor();
